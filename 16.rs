2c32094fdc2e52289210cbc3f55e9bec79db40a1
Erik Bernhardsson <mail@erikbern.com>
2021-12-19 13:22:32 -0500

    Day 16 – part 2

--- 16.rs
+++ 16.rs
@@ -1,6 +1,7 @@
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::env;
+use std::cmp::{min, max};
 
 fn read_bits(a: &[u8], n: usize) -> (usize, &[u8]) {
   let mut ret: usize = 0;
@@ -24,40 +25,54 @@ fn read_literal(a: &[u8]) -> (usize, &[u8]) {
   (l, a_p)
 }
 
-fn read_operator(a: &[u8]) -> (usize, &[u8]) {
+fn read_operator(a: &[u8], type_id: usize) -> (usize, usize, &[u8]) {
   let (lti, a) = read_bits(a, 1);
   let mut version_sum: usize = 0;
+  let mut a_p: &[u8];
+  let mut values = Vec::<usize>::new();
   if lti == 0 {
     let (tl, a) = read_bits(a, 15);
     let mut a_subpackets = &a[..tl];
     while a_subpackets.len() > 0 {
-      let (v_sum, a_new) = read_packet(a_subpackets);
+      let (v_sum, value, a_new) = read_packet(a_subpackets);
       a_subpackets = a_new;
       version_sum += v_sum;
+      values.push(value);
     }
-    (version_sum, &a[tl..])
+    a_p = &a[tl..];
   } else {
-    let (n_subpackets, a) = read_bits(a, 11);
-    let mut a_p = &a[..];
+    let (n_subpackets, a_new) = read_bits(a, 11);
+    a_p = a_new;
     for _i in 0..n_subpackets {
-      let (v_sum, a_new) = read_packet(a_p);
+      let (v_sum, value, a_new) = read_packet(a_p);
       a_p = a_new;
       version_sum += v_sum;
+      values.push(value);
     }
-    (version_sum, a_p)
   }
+  let value = match type_id {
+    0 => values.iter().fold(0, |a, b| a + b),
+    1 => values.iter().fold(1, |a, b| a * b),
+    2 => values.iter().fold(usize::MAX, |a, b| min(a, *b)),
+    3 => values.iter().fold(0, |a, b| max(a, *b)),
+    5 => (values[0] > values[1]) as usize,
+    6 => (values[0] < values[1]) as usize,
+    7 => (values[0] == values[1]) as usize,
+    _ => 999999999,
+  };
+  println!("    values = {:?} -> {}", values, value);
+  (version_sum, value, a_p)
 }
 
-fn read_packet(a: &[u8]) -> (usize, &[u8]) {
+fn read_packet(a: &[u8]) -> (usize, usize,  &[u8]) {
   let (v, a) = read_bits(a, 3);
   let (t, a) = read_bits(a, 3);
-  println!("version = {} type = {}", v, t);
   if t == 4 {
-    let (_, a) = read_literal(a);
-    (v, a)
+    let (value, a) = read_literal(a);
+    (v, value, a)
   } else {
-    let (version_sum, a) = read_operator(a);
-    (v + version_sum, a)
+    let (version_sum, value, a) = read_operator(a, t);
+    (v + version_sum, value, a)
   }
 }
 
@@ -70,9 +85,9 @@ fn handle(line: &String) {
       a.push((m >> (3-i)) & 1);
     }
   }
-  println!("{:?}", a);
-  let (version_sum, _) = read_packet(&a[..]);
-  println!("version sum = {}", version_sum);
+  println!("    {:?}", a);
+  let (version_sum, value, _) = read_packet(&a[..]);
+  println!("    version sum = {} value = {}", version_sum, value);
 }
 
 fn main() {
