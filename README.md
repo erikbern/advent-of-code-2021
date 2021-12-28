c66a76048a1397f2b90fae0de4d807b2ce5afc80
Erik Bernhardsson <mail@erikbern.com>
2021-12-27 21:27:57 -0500

    README

---
+++ README.md
@@ -0,0 +1,2 @@
+Number of lines of solution as a function of the day (1-25):
+![num lines](https://raw.github.com/erikbern/advent-of-code-2021/main/plot.png)
--- plot.py
+++ plot.py
@@ -2,7 +2,7 @@ from matplotlib import pyplot
 
 n_lines = [len([line for line in open('src/bin/%02d.rs' % i)]) for i in range(1, 26)]
 pyplot.style.use('ggplot')
-pyplot.plot(n_lines, '.:')
+pyplot.plot(range(1, 26), n_lines, '.:')
 pyplot.ylabel('Number of lines')
 pyplot.xlabel('Day')
 pyplot.savefig('plot.png', dpi=288)
